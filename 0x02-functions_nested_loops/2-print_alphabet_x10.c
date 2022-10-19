a81f21f18aa190559ff174bdc129d9b411732b44
joseph kouadio <josephkouadio82@gmail.com>
2022-10-19 08:24:15 -0700

    10 times alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print times in alpahabet in lowercase followed by new line
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alph <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+
