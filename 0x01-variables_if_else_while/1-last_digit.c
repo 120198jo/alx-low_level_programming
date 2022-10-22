46fed5a2be2465f6cc1b5049f7a8a8df55d4e65c
joseph kouadio <josephkouadio82@gmail.com>
2022-10-21 20:58:41 -0700

    task7

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,28 +3,30 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry point
+ * @void: Null value
+ *
+ * Description: Check the last digit of the number
+ * Return: Zero value to succesful
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 == 5)
+	if (n % 10 == 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	else if (n % 10 < 6)
 	{
-		printf("last digit of %d is %d and less than 6 and not 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-
-	else if (n % 10 > 0)
-
+	else if (n % 10 > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	return (0);
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,34 @@
 #include "main.h"
+
 /**
-*print_line - prints a straight line
-*@n:parameter
-*Return:returns nothing
-*/
-void print_line(int n)
+ * print_diagonal - entry point
+ *
+ * Description: Prints diagonals
+ *@n: number of spaces
+ * Return: void
+ */
+
+void print_diagonal(int n)
 {
-while (n-- > 0)
-{
-_putchar('_');
-}
-_putchar('\n');
+	int row, num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= n; row++)
+		{
+			if (row > 1)
+			{
+				for (num_lines = 1; num_lines <= row - 1; num_lines++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
