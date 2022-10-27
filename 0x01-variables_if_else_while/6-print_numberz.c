bfd638bb847a12596456a5d484208957268a6f63
joseph kouadio <josephkouadio82@gmail.com>
2022-10-26 19:02:19 -0700

    strncpy

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main- Entry point
  * @void: Null value
  *
- * Description: print numbers from 0 to 9 
- * Return: Zero value 
+ * Description: Print numbers from 0 to 9
+ * Return: Zero value
  */
 
 int main(void)
 {
 	int n;
 
-	for (n = '0' ; n <= '9' ; n++)
-
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
 	putchar('\n');
 	return (0);
