b2a2b9fb6707332e5657dcf0a4cd85079bf19080
joseph kouadio <josephkouadio82@gmail.com>
2022-11-07 18:13:37 -0800

    what you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * @argc: count arguments
  * @argv: Arguments
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc - 1);
+	(void) agrc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
