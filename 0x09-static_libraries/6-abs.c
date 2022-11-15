f9200dc6e8c1ee66da473e7b8c199c830ea7a4f2
joseph kouadio <josephkouadio82@gmail.com>
2022-11-15 13:56:43 -0800

    luxury

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: character to be checked
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = n;
+	}
+	else
+	{
+		n = n * -1;
+	}
+	return (n);
+}
